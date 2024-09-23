92f4f937d398926823ac3e646a6e9c8637e4cf16
Ivan Demchenko <idemchenko.js@gmail.com>
2024-09-23 15:58:16 +0200

    fix: :lipstick: choose better icons

--- src/Goal.elm
+++ src/Goal.elm
@@ -251,7 +251,7 @@ renderTrackingDialog : Time.Posix -> Goal -> Html Msg
 renderTrackingDialog now goal =
     div
         [ classList
-            [ ( "flex flex-col justify-center p-2 absolute w-full h-full text-center bg-white/70 backdrop-blur-sm", True )
+            [ ( "flex flex-col justify-center items-center p-2 absolute w-full h-full text-center bg-white/70 backdrop-blur-sm", True )
             , ( "hidden", not goal.ui.showTrackingDialog )
             ]
         ]
@@ -455,28 +455,32 @@ plusIcon =
         , SvgAttr.fill "none"
         ]
         [ Svg.g
-            [ SvgAttr.id "SVGRepo_bgCarrier"
-            , SvgAttr.strokeWidth "0"
+            [ SvgAttr.strokeWidth "0"
             ]
             []
         , Svg.g
-            [ SvgAttr.id "SVGRepo_tracerCarrier"
-            , SvgAttr.strokeLinecap "round"
+            [ SvgAttr.strokeLinecap "round"
             , SvgAttr.strokeLinejoin "round"
             ]
             []
         , Svg.g
-            [ SvgAttr.id "SVGRepo_iconCarrier"
-            ]
+            []
             [ path
-                [ SvgAttr.opacity "0.5"
-                , SvgAttr.d "M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z"
-                , SvgAttr.fill "#1C274C"
+                [ SvgAttr.d "M7 13L10 16L17 9"
+                , SvgAttr.stroke "#000000"
+                , SvgAttr.strokeWidth "2"
+                , SvgAttr.strokeLinecap "round"
+                , SvgAttr.strokeLinejoin "round"
                 ]
                 []
-            , path
-                [ SvgAttr.d "M16.0303 8.96967C16.3232 9.26256 16.3232 9.73744 16.0303 10.0303L11.0303 15.0303C10.7374 15.3232 10.2626 15.3232 9.96967 15.0303L7.96967 13.0303C7.67678 12.7374 7.67678 12.2626 7.96967 11.9697C8.26256 11.6768 8.73744 11.6768 9.03033 11.9697L10.5 13.4393L12.7348 11.2045L14.9697 8.96967C15.2626 8.67678 15.7374 8.67678 16.0303 8.96967Z"
-                , SvgAttr.fill "#1C274C"
+            , Svg.circle
+                [ SvgAttr.cx "12"
+                , SvgAttr.cy "12"
+                , SvgAttr.r "9"
+                , SvgAttr.stroke "#000000"
+                , SvgAttr.strokeWidth "2"
+                , SvgAttr.strokeLinecap "round"
+                , SvgAttr.strokeLinejoin "round"
                 ]
                 []
             ]
@@ -489,44 +493,57 @@ deleteIcon =
         [ SvgAttr.width "24px"
         , SvgAttr.height "24px"
         , SvgAttr.viewBox "0 0 24 24"
+        , SvgAttr.fill "none"
         ]
         [ Svg.g
-            [ SvgAttr.id "SVGRepo_bgCarrier"
-            , SvgAttr.strokeWidth "0"
+            [ SvgAttr.strokeWidth "0"
             ]
             []
         , Svg.g
-            [ SvgAttr.id "SVGRepo_tracerCarrier"
-            , SvgAttr.strokeLinecap "round"
+            [ SvgAttr.strokeLinecap "round"
             , SvgAttr.strokeLinejoin "round"
             ]
             []
         , Svg.g
-            [ SvgAttr.id "SVGRepo_iconCarrier"
-            ]
+            []
             [ path
-                [ SvgAttr.d "M3 6.38597C3 5.90152 3.34538 5.50879 3.77143 5.50879L6.43567 5.50832C6.96502 5.49306 7.43202 5.11033 7.61214 4.54412C7.61688 4.52923 7.62232 4.51087 7.64185 4.44424L7.75665 4.05256C7.8269 3.81241 7.8881 3.60318 7.97375 3.41617C8.31209 2.67736 8.93808 2.16432 9.66147 2.03297C9.84457 1.99972 10.0385 1.99986 10.2611 2.00002H13.7391C13.9617 1.99986 14.1556 1.99972 14.3387 2.03297C15.0621 2.16432 15.6881 2.67736 16.0264 3.41617C16.1121 3.60318 16.1733 3.81241 16.2435 4.05256L16.3583 4.44424C16.3778 4.51087 16.3833 4.52923 16.388 4.54412C16.5682 5.11033 17.1278 5.49353 17.6571 5.50879H20.2286C20.6546 5.50879 21 5.90152 21 6.38597C21 6.87043 20.6546 7.26316 20.2286 7.26316H3.77143C3.34538 7.26316 3 6.87043 3 6.38597Z"
-                , SvgAttr.fill "#1C274C"
+                [ SvgAttr.d "M10 11V17"
+                , SvgAttr.stroke "#000000"
+                , SvgAttr.strokeWidth "2"
+                , SvgAttr.strokeLinecap "round"
+                , SvgAttr.strokeLinejoin "round"
                 ]
                 []
             , path
-                [ SvgAttr.fillRule "evenodd"
-                , SvgAttr.clipRule "evenodd"
-                , SvgAttr.d "M9.42543 11.4815C9.83759 11.4381 10.2051 11.7547 10.2463 12.1885L10.7463 17.4517C10.7875 17.8855 10.4868 18.2724 10.0747 18.3158C9.66253 18.3592 9.29499 18.0426 9.25378 17.6088L8.75378 12.3456C8.71256 11.9118 9.01327 11.5249 9.42543 11.4815Z"
-                , SvgAttr.fill "#1C274C"
+                [ SvgAttr.d "M14 11V17"
+                , SvgAttr.stroke "#000000"
+                , SvgAttr.strokeWidth "2"
+                , SvgAttr.strokeLinecap "round"
+                , SvgAttr.strokeLinejoin "round"
                 ]
                 []
             , path
-                [ SvgAttr.fillRule "evenodd"
-                , SvgAttr.clipRule "evenodd"
-                , SvgAttr.d "M14.5747 11.4815C14.9868 11.5249 15.2875 11.9118 15.2463 12.3456L14.7463 17.6088C14.7051 18.0426 14.3376 18.3592 13.9254 18.3158C13.5133 18.2724 13.2126 17.8855 13.2538 17.4517L13.7538 12.1885C13.795 11.7547 14.1625 11.4381 14.5747 11.4815Z"
-                , SvgAttr.fill "#1C274C"
+                [ SvgAttr.d "M4 7H20"
+                , SvgAttr.stroke "#000000"
+                , SvgAttr.strokeWidth "2"
+                , SvgAttr.strokeLinecap "round"
+                , SvgAttr.strokeLinejoin "round"
                 ]
                 []
             , path
-                [ SvgAttr.opacity "0.5"
-                , SvgAttr.d "M11.5956 22.0001H12.4044C15.1871 22.0001 16.5785 22.0001 17.4831 21.1142C18.3878 20.2283 18.4803 18.7751 18.6654 15.8686L18.9321 11.6807C19.0326 10.1037 19.0828 9.31524 18.6289 8.81558C18.1751 8.31592 17.4087 8.31592 15.876 8.31592H8.12405C6.59127 8.31592 5.82488 8.31592 5.37105 8.81558C4.91722 9.31524 4.96744 10.1037 5.06788 11.6807L5.33459 15.8686C5.5197 18.7751 5.61225 20.2283 6.51689 21.1142C7.42153 22.0001 8.81289 22.0001 11.5956 22.0001Z"
-                , SvgAttr.fill "#1C274C"
+                [ SvgAttr.d "M6 7H12H18V18C18 19.6569 16.6569 21 15 21H9C7.34315 21 6 19.6569 6 18V7Z"
+                , SvgAttr.stroke "#000000"
+                , SvgAttr.strokeWidth "2"
+                , SvgAttr.strokeLinecap "round"
+                , SvgAttr.strokeLinejoin "round"
+                ]
+                []
+            , path
+                [ SvgAttr.d "M9 5C9 3.89543 9.89543 3 11 3H13C14.1046 3 15 3.89543 15 5V7H9V5Z"
+                , SvgAttr.stroke "#000000"
+                , SvgAttr.strokeWidth "2"
+                , SvgAttr.strokeLinecap "round"
+                , SvgAttr.strokeLinejoin "round"
                 ]
                 []
             ]
