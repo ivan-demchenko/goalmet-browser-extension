278c48af807803bc6b68f4941e46d2b1834e9563
Ivan Demchenko <idemchenko.js@gmail.com>
2024-10-03 14:36:37 +0200

    chore: code cleanup

--- src/DayNote.elm
+++ src/DayNote.elm
@@ -4,34 +4,13 @@ import DataModel exposing (TrackingEntry)
 import Html exposing (Html, button, div, span, text)
 import Html.Attributes exposing (class)
 import Html.Events exposing (onClick)
-import Json.Decode as JD
-import Json.Encode as JE
 import Time
 import Utils
-
-
-decoder : JD.Decoder TrackingEntry
-decoder =
-    JD.map2 TrackingEntry
-        (JD.field "timestamp" <| JD.map Time.millisToPosix JD.int)
-        (JD.field "note" JD.string)
-
-
-encoder : TrackingEntry -> JE.Value
-encoder model =
-    JE.object
-        [ ( "timestamp", JE.int <| Time.posixToMillis model.timestamp )
-        , ( "note", JE.string model.note )
-        ]
 
 
 getTimestamp : TrackingEntry -> Time.Posix
 getTimestamp =
     .timestamp
-
-
-
--- type Msg = UpdateNote String
 
 
 view : (Time.Posix -> msg) -> TrackingEntry -> Html msg
